027366e6f626c4cbfb74ab3e08df03a8e673f263
Bartosz Cholewa <bbcholewa@gmail.com>
2026-01-28 23:23:33 +0100

    Remove bullet after hit close #1

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -1,15 +1,17 @@
 class_name Bullet
 extends Node2D
 
-const SPEED: int = 100
+const SPEED: int = 1600
 
 @onready var life_timer: Timer = $LifeTimer
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
 
 var direction: Vector2
 
 
 func _ready() -> void:
 	life_timer.timeout.connect(_on_life_timer_timeout)
+	hitbox_component.hit_hurtbox.connect(_on_hit_hurtbox)
 
 func _physics_process(delta: float) -> void:
 	global_position += direction * SPEED * delta
@@ -18,5 +20,11 @@ func start(bullet_direction: Vector2) -> void:
 	direction = bullet_direction
 	rotation = direction.angle()
 
+func register_collision() -> void:
+	queue_free()
+
 func _on_life_timer_timeout() -> void:
 	queue_free()
+
+func _on_hit_hurtbox(_hurtbox_component: HurtboxComponent) -> void:
+	register_collision()
